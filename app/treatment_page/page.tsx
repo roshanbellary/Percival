a031a5e96cc6cb1358e984be69ed44b97676a819
Roshan Bellary <roshan.bellary@gmail.com>
2024-10-05 17:02:06 -0400

    Patient History

---
+++ app/treatment_page/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { useState, useEffect, useRef } from "react";
+
+export default function TreatmentPage() {
+    return (
+        <div>
+            hi
+        </div>
+    );
+}
